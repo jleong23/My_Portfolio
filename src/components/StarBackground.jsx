fa7b60b14221af60198978a28e0111d7288876a9
Jason Leong <jleongdh@gmail.com>
2025-06-26 13:09:29 +0930

    Finish star + meteor background

--- src/components/StarBackground.jsx
+++ src/components/StarBackground.jsx
@@ -1,12 +1,22 @@
 import { useEffect, useState } from "react"
 
-// id, size, x, y, opacity, animationDuration
-
 export const StarBackground = () => {
   const [stars, setStars] = useState([]);
+  // id, size, x, y, opacity, animationDuration
+  const [meteors, setMeteors] = useState([]);
+  // id, size, x, y, delay, animationDuration
 
   useEffect(() => {
     generateStars();
+    generateMeteors();
+
+    const handleResize = () => {
+      generateStars();
+    };
+
+    window.addEventListener("resize", handleResize)
+
+    return () => window.removeEventListener("resize", handleResize);
   },[]);
 
   const generateStars = () => {
@@ -26,6 +36,24 @@ export const StarBackground = () => {
   }
   setStars(newStars);
   };
+
+  const generateMeteors = () => {
+    const numberOfMeteors = 4;
+
+    const newMeteors = []
+
+  for (let i=0; i<numberOfMeteors; i++){
+    newMeteors.push({
+      id: i,
+      size: Math.random() * 2 + 1,
+      x: Math.random() * 100,
+      y: Math.random() * 20,
+      delay: Math.random() * 15,
+      animationDuration: Math.random() * 4 + 2,
+    });
+  }
+  setMeteors(newMeteors);
+  };
   return (
   <div className="fix inset-0 overflow-hidden pointer-events-none z-0">
     {stars.map((star) => (
@@ -36,6 +64,17 @@ export const StarBackground = () => {
         top: star.y + "%" ,
         opacity: star.opacity ,
         animationDuration: star.animationDuration + "s",
+      }} />
+    ))}
+
+    {meteors.map((meteor) => (
+      <div key={meteor.id} className="meteor animate-meteor" style={{
+        width: meteor.size * 50 + "px",
+        height: meteor.size * 3 + "px" ,
+        left: meteor.x + "%",
+        top: meteor.y + "%" ,
+        animationDelay: meteor.delay ,
+        animationDuration: meteor.animationDuration + "s",
       }} />
     ))}
   </div>
