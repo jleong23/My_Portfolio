3969b63073d5874482c7ff79bd2d122b5bcfc033
Jason Leong <jleongdh@gmail.com>
2025-07-04 11:38:15 +0930

    Complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { NotFound } from "./pages/NotFound";
 import { Home } from "./pages/Home";
-import { Toaster } from "./components/ui/toaster";
+import { Toaster } from "./components/ui/toaster"
+
+
 
 function App() {
   return (
